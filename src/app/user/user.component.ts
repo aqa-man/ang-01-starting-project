f49209a60c132026948f318762a3878166f07755
AQA <automatedqualityassurance@gmail.com>
2025-06-23 16:07:34 -0700

    June 23, 2025 lesson work

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,13 +9,26 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  // decorator approach
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
 
-    this.selectedUser = DUMMY_USERS[randomIndex];
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+
+  // signal approach --> must call as a function with ()'s
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
 }
